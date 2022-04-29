b121550dfdebfd59e5b4cdc19aa1a9dc9e8e73a0
nicolotombini <nicolo.tombini@gmail.com>
2022-04-29 10:48:18 +0200

    dfsRGADTF

--- backend/index.js
+++ backend/index.js
@@ -20,27 +20,29 @@ const userResolvers = {};
 
 app.use(cors())
 
-userResolvers['pippo'] = new Resolver();
-userResolvers['pluto'] = new Resolver();
-
 app.get('/', (req, res) => {
   res.send("Hello World");
 })
 
 // da hostname a indirizzo IpV4
-app.get('/resolve/a/:hostname', (req, res) => {
+app.get(':username/resolve/a/:hostname', (req, res) => {
+  var username = req.params.username;
+  var resolver = new Resolver();
   var hostname = req.params.hostname;
-  userResolvers['pippo'].resolve4(hostname, function (err, addresses, family) {
-    res.status(200).send(addresses);
 
+  resolver.resolve4(hostname, function (err, addresses, family) {
+    res.status(200).send(addresses);
     console.log(err);
   });
 })
 
 // da hostname a indirizzo IpV6
-app.get('/resolve/aaaa/:hostname', (req, res) => {
+app.get(':username/resolve/aaaa/:hostname', (req, res) => {
   var hostname = req.params.hostname;
-  userResolvers['pippo'].resolve6(hostname, function (err, addresses, family) {
+  var username = req.params.username;
+  var resolver = new Resolver();
+
+  resolver.resolve6(hostname, function (err, addresses, family) {
     res.status(200).send(addresses);
     console.log(err);
   });
@@ -48,103 +50,134 @@ app.get('/resolve/aaaa/:hostname', (req, res) => {
 
 // qua ci va resolve any
 
-app.get('/resolve/soa/:domain', (req, res) => {
+app.get(':username/resolve/soa/:domain', (req, res) => {
   var domain = req.params.domain;
-  userResolvers['pippo'].resolveSoa(domain, function (err, address) {
+  var username = req.params.username;
+  var resolver = new Resolver();
+
+  resolver.resolveSoa(domain, function (err, address) {
     res.status(200).send(address);
     console.log(err);
   });
 })
 
 // da IpV4 o IpV6 ad hostname
-app.get('/resolve/ip/:ip', (req, res) => {
+app.get(':username/resolve/ip/:ip', (req, res) => {
   var ip = req.params.ip;
-  userResolvers['pippo'].reverse(ip, function (err, hostnames) {
+  var username = req.params.username;
+  var resolver = new Resolver();
+
+  resolver.reverse(ip, function (err, hostnames) {
     res.status(200).send(hostnames);
     console.log(err);
   });
 })
 
 // ritorna CA per un record dns
-app.get('/resolve/caa/:domain', (req, res) => {
+app.get(':username/resolve/caa/:domain', (req, res) => {
   var domain = req.params.domain;
-  userResolvers['pippo'].resolveCaa(domain, function (err, caas) {
+  var username = req.params.username;
+  var resolver = new Resolver();
+
+  resolver.resolveCaa(domain, function (err, caas) {
     res.status(200).send(caas);
     console.log(err);
   });
 })
 
 // Risolve indirizzo IP e porta in un hostname e in un servizio attivo su una porta
-app.get('/lookup/service/:ip/:port', (req, res) => {
+app.get(':username/lookup/service/:ip/:port', (req, res) => {
   var ip = req.params.ip;
   var port = req.params.port;
+
   dns.lookupService(ip, parseInt(port), function (err, hostname, service) {
       res.status(200).send(hostname + " " + service);
   });
 })
 
-app.get('/resolve/cname/:hostname', (req, res) => {
+app.get(':username/resolve/cname/:hostname', (req, res) => {
   var hostname = req.params.hostname;
-  userResolvers['pippo'].resolveCname(hostname, function (err, addresses) {
+  var username = req.params.username;
+  var resolver = new Resolver();
+
+  resolver.resolveCname(hostname, function (err, addresses) {
     res.status(200).send(addresses);
     console.log(err);
   });
 })
 
 // Torna i record mx per un certo dominio
-app.get('/resolve/mx/:domain', (req, res) => {
+app.get(':username/resolve/mx/:domain', (req, res) => {
   var domain = req.params.domain;
-  userResolvers['pippo'].resolveMx(domain, function (err, names) {
+  var username = req.params.username;
+  var resolver = new Resolver();
+
+  resolver.resolveMx(domain, function (err, names) {
     res.status(200).send(names);
     console.log(err);
   });
 })
 
 // name server disponibili per un hostname
-app.get('/resolve/ns/:domain', (req, res) => {
+app.get(':username/resolve/ns/:domain', (req, res) => {
   var domain = req.params.domain;
-  userResolvers['pippo'].resolveNs(domain, function (err, addresses) {
+  var username = req.params.username;
+  var resolver = new Resolver();
+
+  resolver.resolveNs(domain, function (err, addresses) {
     res.status(200).send(addresses);
     console.log(err);
   });
 })
 
 // name server disponibili per un hostname
-app.get('/resolve/srv/:hostname', (req, res) => {
+app.get(':username/resolve/srv/:hostname', (req, res) => {
   var hostname = req.params.hostname;
-  userResolvers['pippo'].resolveSrv(hostname, function (err, addresses) {
+  var username = req.params.username;
+  var resolver = new Resolver();
+
+  resolver.resolveSrv(hostname, function (err, addresses) {
     res.status(200).send(addresses);
     console.log(err);
   });
 })
 
-app.get('/resolve/ptr/:hostname', (req, res) => {
+app.get(':username/resolve/ptr/:hostname', (req, res) => {
   var hostname = req.params.hostname;
-  userResolvers['pippo'].resolvePtr(hostname, function (err, addresses) {
+  var username = req.params.username;
+  var resolver = new Resolver();
+
+  username.resolvePtr(hostname, function (err, addresses) {
     res.status(200).send(addresses);
     console.log(err);
   });
 })
 
 // Torna alcune informazioni
-app.get('/resolve/txt/:hostname', (req, res) => {
+app.get(':username/resolve/txt/:hostname', (req, res) => {
   var hostname = req.params.hostname;
-  userResolvers['pippo'].resolveTxt(hostname, function (err, addresses) {
+  var username = req.params.username;
+  var resolver = new Resolver();
+
+  username.resolveTxt(hostname, function (err, addresses) {
     res.status(200).send(addresses);
     console.log(err);
   });
 })
 
 // Ritorna i server impostati per il resolver
-app.get('/config/servers/:username', (req, res) => {
-    var username = req.params.username;
-    var servers = userResolvers[username].getServers();
-    res.send(servers);
+app.get(':username/config/servers', (req, res) => {
+  var username = req.params.username;
+  var resolver = new Resolver();
+
+  var servers = userResolvers[username].getServers();
+  res.send(servers);
 })
 
 // Setta un name server per la risoluzione
-app.post('/config/servers/:username', (req, res) => {
-  var username = req.body.username;
+app.post(':username/config/servers', (req, res) => {
+  var username = req.params.username;
+  var resolver = new Resolver();
   userResolvers[username].setServers(
     [
       '8.8.8.8',
